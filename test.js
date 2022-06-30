54936f3b57a6f6d2dc53fdf25881425e368ff1d9
laxmi koirala <koiralalaxmi349@gmail.com>
2022-06-30 09:25:27 +0545

    js

---
+++ test.js
@@ -0,0 +1,46 @@
+// function helloworld()
+// {
+//     console.log("Hello world")
+// // }
+// helloworld();
+
+// function test(a,b)
+// {
+//    return(a+b)
+
+    
+// }
+
+// console.log("the value is", test(2,3));
+
+let a = {name: 'laxmi'}
+
+
+console.log(a.name);
+
+for(i=0;i<4;i++){
+    console.log(i)
+}
+
+const array = ["mangos, banana, graps"];
+for(let i=0; i<array.length;i++)
+{
+    console.log(array[i]);
+}
+
+let A={name:'laxmi'}
+console.log(a.name);
+
+function test()
+{
+    x=10 , y=12;
+    if(x===y)
+    {
+        console.log("Equal")
+    }
+    else{
+        console.log("Not Equals to")
+    }
+}
+test();
+
